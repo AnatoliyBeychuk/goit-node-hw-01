dcf0af14b3ada12c536934cf2f104e20c692208c
AnatoliyBeychuk <abis01@ukr.net>
2022-03-14 16:42:54 +0200

    Домашнее задание №1

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # goit-node-hw-01
+
+## Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+<code>node index.js --action list</code>
+
+<a href="https://ibb.co/rtVFSsr"><img src="https://i.ibb.co/W3LGwtJ/Screenshot-1.png" alt="Screenshot-1" border="0"></a>
+
+## Получаем контакт по id
+
+<code>node index.js --action get --id 5</code>
+
+<a href="https://ibb.co/qgwGKTG"><img src="https://i.ibb.co/n1S29x2/Screenshot-2.png" alt="Screenshot-2" border="0"></a>
+
+## Добавялем контакт
+
+<code>node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22</code>
+
+<a href="https://ibb.co/HtypyFB"><img src="https://i.ibb.co/x1b8bCX/Screenshot-3.png" alt="Screenshot-3" border="0"></a>
+
+## Удаляем контакт
+
+<code>node index.js --action remove --id=3</code>
+
+<a href="https://ibb.co/p3Cz7LV"><img src="https://i.ibb.co/y8Ly9Qv/Screenshot-4.png" alt="Screenshot-4" border="0"></a>
---
+++ contacts.js
@@ -0,0 +1,47 @@
+const { v4 } = require("uuid");
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  if (!contact) return null;
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) return null;
+  const [removedContact] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return removedContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const contact = contacts.find(
+    (contact) =>
+      contact.phone.replace(/[^\d]/g, "") === phone.replace(/[^\d]/g, "")
+  );
+  if (contact) return null;
+  const newContact = { id: v4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
